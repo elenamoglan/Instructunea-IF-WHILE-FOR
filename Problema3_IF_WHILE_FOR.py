f2af6ee22cb911e88ed9f438e92b9bb003dd04e5
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2020-11-18 10:31:48 +0300

    Add files via upload

---
+++ Problema3_IF_WHILE_FOR.py
@@ -0,0 +1,8 @@
+n=int(input('Introducet n: '))
+m=int(input('Introduceti m: '))
+while n%m==0:
+    n=n//m
+if n==1:
+    print('Numaru n este o putere a lui m')
+else:
+    print('Numaru n este o putere a lui m')
